2bbde0e9bd30bf6fa3852a5847e013b3df50fe2b
TalkToVictor <itsvictornwani@yahoo.com>
2021-03-27 13:42:32 -0400

    the committer

--- kata.js
+++ kata.js
@@ -27,3 +27,62 @@ function checkExam(array1, array2) {
         return score;
     }
 }
+
+// Imagine you are in a universe where you can just perform the following arithematic operations. Addition(+), Subtraction(-), Multiplication(*), Division(/). You are given given a postfix expression. Postfix expression is where operands come after operator. Each operator and operand are seperated by a space. You need to evaluate the expression.
+
+// For example: 25 45 + is equivalent of 25 + 45, hence the answer would be 70. Instead if you are given 20 40 + 60 *, it is equivalent of (20+40) * 60, hence the answer should be 3600. 20 40 60 + * is equivalent of 20 * (40 + 60) = 2000.
+
+// Create a method 'evaluate' that takes a string as input and returns a long resulted in the evaluation. Just concentrate on happy paths. Assume that expression is always valid and division is always an integer division.
+
+
+const operatorMap = {
+    ["+"]: function (a, b) {
+        return a + b;
+    },
+    ["-"]: function (a, b) {
+        return a - b;
+    },
+    ["*"]: function (a, b) {
+        return a * b;
+    },
+    ["/"]: function (a, b) {
+        return parseInt(a / b);
+    }, // The solution explicitly states integer division
+};
+function postfixEvaluator(string) {
+    const split = string.split(" ");
+    console.log("Starting with expression:", split);
+
+    for (let i = 0; i < split.length; i++) {
+        if (split.length == 1) {
+            // We've finished all calculations, this must be our value
+            return parseInt(split[0]);
+        }
+
+        console.log("-----------");
+        if (!parseInt(split[i + 2])) {
+            // There's an operator 2 spaces ahead, calculate now!
+            const leftNum = parseInt(split[i]);
+            const rightNum = parseInt(split[i + 1]);
+            const operatorStr = split[i + 2];
+
+            // Get a function based on its string representation
+            const operatorFn = operatorMap[operatorStr];
+
+            console.log(
+                "Evaluating subexpression:",
+                `i=${i} to i=${i + 2} as`,
+                split.slice(i, i + 3)
+            );
+            const value = operatorFn(leftNum, rightNum);
+            console.log("\tsubexpression value:", value);
+
+            // Modify the array in-place
+            split.splice(i, 3, value);
+            console.log("Complete expression is now:", split);
+
+            i -= 2; // Move the loop backwards
+        }
+    }
+
+    return split[0]
