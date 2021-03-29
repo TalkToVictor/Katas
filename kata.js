9ccf0f7ca135ddcc4ca573575c86dde0a660055f
TalkToVictor <itsvictornwani@yahoo.com>
2021-03-29 15:46:45 -0400

    the committer

--- kata.js
+++ kata.js
@@ -142,3 +142,36 @@ function postfixEvaluator(string) {
         }
         return str.slice(1);
     }
+
+
+    // Uh oh, Someone at the office has dropped all these sequences on the floor and forgotten to label them with their correct bases.
+
+    // We have to fix this before the boss gets back or we're all going to be fired!
+
+    // This is what your years of coding have been leading up to, now is your time to shine!
+
+    // Task
+    // You will have to create a function which takes in a sequence of numbers in random order and you will have to return the correct base of those numbers.
+
+    // The base is the number of unique digits. For example, a base 10 number can have 10 unique digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 and a base 2 number (Binary) can have 2 unique digits: 0 and 1.
+
+    // Constraints
+    // The sequence will always be 10 numbers long and we know that the base is going to be between 2 and 10 inclusive so no need to worry about any letters. When sorted, the sequence is made up of consecutive numbers.
+
+    // https://www.codewars.com/kata/5f47e79e18330d001a195b55
+
+
+    //TRi solution
+    function baseFinder(seq) {
+        let arr = [];
+        let baseArr = [];
+        arr = seq.map((x) => x.split(""));
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+                if (!baseArr.includes(arr[i][j])) {
+                    baseArr.push(arr[i][j]);
+                }
+            }
+        }
+        return baseArr.length;
+    }
