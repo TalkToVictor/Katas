4b961bcc712e450c7e1ea3b56625b26ff8e3ce6d
TalkToVictor <itsvictornwani@yahoo.com>
2021-04-08 15:52:50 -0400

    the committer

--- kata.js
+++ kata.js
@@ -961,3 +961,9 @@ function postfixEvaluator(string) {
 var breakChocolate = function (n, m) {
     return Math.max(0, m * n - 1);
 };
+
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+
+const moveZeros = arr => arr.filter(i => i !== 0).concat(arr.filter(i => i === 0))
