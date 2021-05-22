6cb3f6a8a2fdebe24ded2ffd956c3467f2c63c3b
TalkToVictor <itsvictornwani@yahoo.com>
2021-05-22 13:49:53 -0400

    the committer

--- kata.js
+++ kata.js
@@ -1593,3 +1593,16 @@ function repeats(arr) {
 };
 
 
+//solution
+function repeats(arr) {
+    let equation = []
+    let sorted = arr.sort((a, b) => a - b)
+
+    for (let i = 0; i < sorted.length; i++) {
+        if (sorted[i] !== sorted[i + 1] && sorted[i] !== sorted[i - 1]) {
+            equation.push(sorted[i])
+        }
+    }
+
+    return equation.reduce((acc, num) => acc + num)
+};
