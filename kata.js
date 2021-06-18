e98f17d43d9d09b4c526827f495a79a1937585e6
TalkToVictor <itsvictornwani@yahoo.com>
2021-06-18 16:49:10 -0400

    the committer

--- kata.js
+++ kata.js
@@ -2066,3 +2066,14 @@ function repeats(arr) {
     }
     return unq.reduce((ac, v) => ac + v, 0)
 };
+
+// solution 
+function repeats(arr) {
+    var result = {};
+    var array = [];
+    arr.forEach(item => result[item] ? result[item]++ : result[item] = 1);
+    array = Object.entries(result);
+    array = array.filter(valor => valor.includes(1));
+    array = array.map(item => item[0]).reduce((a, b) => Number(a) + Number(b));
+    return array;
+};
