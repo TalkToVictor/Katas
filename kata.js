777a5e2dcdbac59832d4212a9e35c3c52da4129b
TalkToVictor <itsvictornwani@yahoo.com>
2021-07-11 15:55:22 -0400

    the committer

--- kata.js
+++ kata.js
@@ -2373,3 +2373,18 @@ function repeats(arr) {
     arr.forEach(x => set.has(x) ? set.delete(x) : set.add(x))
     return [...set].reduce((s, v) => s + v, 0)
 }
+
+// solution
+
+function repeats(arr) {
+    var helper = {};
+    for (var i = 0; i < arr.length; ++i) {
+        if (helper[arr[i]] === undefined) {
+            helper[arr[i]] = 1;
+        } else {
+            delete helper[arr[i]];
+        }
+    }
+    var ks = Object.keys(helper);
+    return parseInt(ks[0]) + parseInt(ks[1]);
+};
