03be2f9ffbf2b4a190959be558ae9ba4493007dc
TalkToVictor <itsvictornwani@yahoo.com>
2021-08-03 19:50:06 -0400

    thr push

--- kata.js
+++ kata.js
@@ -2804,3 +2804,19 @@ function repeats(arr) {
     }
     return summa;
 };
+
+//solution 
+
+function repeats(arr) {
+    //..
+    let result = [];
+    let sorted = arr.sort((a, b) => a - b);
+    console.log(arr.sort((a, b) => a - b))
+    for (let i = 0; i < sorted.length; i++) {
+        if (sorted[i] != sorted[i + 1] && sorted[i] != sorted[i - 1]) {
+            result.push(sorted[i])
+        }
+    }
+    console.log(result)
+    return result.reduce((sum, current) => sum + current, 0);
+};
