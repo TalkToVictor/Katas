5a432579a49464a20364c5d27c5efd70bbe9a81a
TalkToVictor <itsvictornwani@yahoo.com>
2021-08-05 12:08:14 -0400

    the committer

--- kata.js
+++ kata.js
@@ -2846,3 +2846,21 @@ function repeats(arr) {
     }
     return summa;
 };
+
+
+// solution 
+
+function repeats(arr) {
+    const copy = [...arr];
+
+    copy.sort((a, b) => a - b);
+
+    for (let i = 0; i < copy.length; i++) {
+        if (copy[i] === copy[i + 1]) {
+            copy[i] = null;
+            copy[i + 1] = null;
+        }
+    }
+
+    return copy.filter((x) => typeof x === 'number').reduce((a, b) => a + b);
+};
