3d62e7e6f8e7f82af538c89dbf0468392659bb12
TalkToVictor <itsvictornwani@yahoo.com>
2021-08-08 17:29:42 -0400

    anticipater

--- kata.js
+++ kata.js
@@ -2529,6 +2529,26 @@ leon mentioned reduce, filter, indexOf; use reduce to create object
         return sum
     }
 
+    // solution
+
+    function repeats(arr) {
+        let numbersRepetitions = {};
+        let sum = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+            let number = arr[i];
+
+            if (!numbersRepetitions[number]) {
+                numbersRepetitions[number] = 1;
+                sum += number;
+            } else {
+                sum -= number;
+            }
+        }
+
+        return sum;
+    };
+
 
 };
 
