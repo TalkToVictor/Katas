b4c9819103d797b3d197e7b1bcdc8738ac971b56
TalkToVictor <itsvictornwani@yahoo.com>
2021-08-23 16:11:31 -0400

    seredebe

--- kata.js
+++ kata.js
@@ -3175,3 +3175,16 @@ function repeats(arr) {
 
 // solution
 
+function repeats(arr) {
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
+            count += arr[i];
+        }
+
+    }
+    return count;
+};
+
+// solution
+
