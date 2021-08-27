8486189423008e1bf1e82887004da59a16ee669f
TalkToVictor <itsvictornwani@yahoo.com>
2021-08-27 18:20:06 -0400

    the committer

--- kata.js
+++ kata.js
@@ -3216,3 +3216,11 @@ function repeats(arr) {
         .filter(x => arr.indexOf(x) === arr.lastIndexOf(x))
         .reduce((a, b) => a + b);
 };
+
+// solution
+
+// i like reduce 
+const repeats = (arr) => [0, ...arr].
+    reduce((acc, el, _, arr) => arr.
+        filter(e => e === el).
+        length < 2 ? acc + el : acc)
