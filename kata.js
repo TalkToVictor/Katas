c0ac7c41257e53aa2cc710c42b27b7ed0f1d17ab
TalkToVictor <itsvictornwani@yahoo.com>
2021-10-27 14:20:11 +0100

    the currenta

--- kata.js
+++ kata.js
@@ -4021,3 +4021,15 @@ function repeats(arr) {
     }
     return res;
 }; S
+
+// Solution
+
+function repeats(arr) {
+    arr.sort();
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i + 1] != arr[i]) newArr.push(arr[i]);
+        else i += 1;
+    }
+    return newArr.reduce((sum, num) => sum + num);
+};
