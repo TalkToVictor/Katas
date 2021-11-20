a1d819d5bd24e7bea444e5e781960d1744ddc40b
TalkToVictor <itsvictornwani@yahoo.com>
2021-11-20 01:30:40 +0100

    the committer

--- kata.js
+++ kata.js
@@ -4380,5 +4380,24 @@ console.log(repeats([4, 5, 7, 5, 4, 8]));                // should return 15
 console.log(repeats([9, 10, 19, 13, 19, 13]));      // should return 19
 console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11])); // should return 12
 
-  // Object.keys(object)   <-- numbers
-  // Object.values(object) <-- count
+// Object.keys(object)   <-- numbers
+// Object.values(object) <-- count
+
+//   solution
+function repeats(arr) {
+    let numbersRepetitions = {};
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let number = arr[i];
+
+        if (!numbersRepetitions[number]) {
+            numbersRepetitions[number] = 1;
+            sum += number;
+        } else {
+            sum -= number;
+        }
+    }
+
+    return sum;
+};
