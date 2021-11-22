c8e1bc2c742719c5495a4e5ad8682f69778f533d
TalkToVictor <itsvictornwani@yahoo.com>
2021-11-22 15:51:18 +0100

    the data

--- kata.js
+++ kata.js
@@ -4421,3 +4421,21 @@ function repeats(arr) {
     }
     return retValue;
 };
+
+// Solution
+
+function repeats(arr) {
+    let i;
+    let sum = 0;
+    arr.sort((a, b) => {
+        return a - b;
+    });
+    for (i = 0; i < arr.length; i += 2) {
+        if (arr[i] !== arr[i + 1]) {
+            sum += arr[i];
+            i--;
+        }
+    }
+
+    return sum;
+};
