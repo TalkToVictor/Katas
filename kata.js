00d9d23e717d9ac9b459b3c8972dd4d2ebbd19a8
TalkToVictor <itsvictornwani@yahoo.com>
2021-12-08 21:44:59 -0500

    the comitter

--- kata.js
+++ kata.js
@@ -4635,3 +4635,15 @@ function repeats(arr) {
         }
     } return sum
 };
+
+// Solution
+
+function repeats(arr) {
+    let n = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.filter((v) => v == arr[i]).length != 2) {
+            n.push(arr[i]);
+        }
+    }
+    return n[0] + n[1]
+}
