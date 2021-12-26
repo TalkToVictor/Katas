51cc0823f93324e22f039a12c1748e00bce31617
TalkToVictor <itsvictornwani@yahoo.com>
2021-12-25 22:02:33 -0500

    the committer

--- kata.js
+++ kata.js
@@ -4824,3 +4824,21 @@ function repeats(arr) {
     return parseInt(ks[0]) + parseInt(ks[1]);
 };
 
+//Solution
+
+function repeats(arr) {
+    let i;
+    let sum = 0;
+    arr.sort((a, b) => {
+        return a - b;
+    });
+    for (i = 0; i < arr.length; i += 2) {
+        if (arr[i] !== arr[i + 1]) {
+            sum += arr[i];
+            i--;
+        }
+    }
+
+    return sum;
+};
+
