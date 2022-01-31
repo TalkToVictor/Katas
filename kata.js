c0ff272eae450725280eb0ee6e7452487ee749ba
Talktovictor <itsvictornwani@yahoo.com>
2022-01-31 00:08:07 -0500

    the commingler

--- kata.js
+++ kata.js
@@ -5229,3 +5229,20 @@ function repeats(arr) {
 
             return uniques.reduce((a, b) => a + b);
         };
+
+        // solution 
+        function repeats(arr) {
+            let i;
+            let sum = 0;
+            arr.sort((a, b) => {
+                return a - b;
+            });
+            for (i = 0; i < arr.length; i += 2) {
+                if (arr[i] !== arr[i + 1]) {
+                    sum += arr[i];
+                    i--;
+                }
+            }
+
+            return sum;
+        };
