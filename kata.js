4345048ec42ec769da1245ecd2da5e60d3e86629
Talktovictor <itsvictornwani@yahoo.com>
2022-02-04 23:41:26 -0500

    the girlie

--- kata.js
+++ kata.js
@@ -5267,3 +5267,19 @@ function repeats(arr) {
             var ks = Object.keys(helper);
             return parseInt(ks[0]) + parseInt(ks[1]);
         };
+
+        //Solution
+        function repeats(numbers) {
+            let sum = 0
+
+            for (const number of numbers) {
+                const firstOccurence = numbers.indexOf(number)
+                const lastOccurence = numbers.lastIndexOf(number)
+
+                if (firstOccurence == lastOccurence) {
+                    sum += number
+                }
+            }
+
+            return sum
+        }
