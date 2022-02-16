69a0154988f0b130923316582a1cf20f2843a210
Talktovictor <itsvictornwani@yahoo.com>
2022-02-15 23:39:13 -0500

    stop procastinating

--- kata.js
+++ kata.js
@@ -5444,3 +5444,15 @@ function repeats(arr) {
                 arr.filter((y) =>
                     // compare with current element
                     y == x).length == 1).reduce((a, b) => a + b);
+
+        // solution
+        const repeats = arr => {
+            let result = 0;
+            for (let i = 0; i < arr.length; i++) {
+                if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
+                    continue;
+                };
+                result += arr[i];
+            };
+            return result;
+        };
