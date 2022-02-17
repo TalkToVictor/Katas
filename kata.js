625fdbc5ee8d4193d4ad99896ef6199e123606b6
Talktovictor <itsvictornwani@yahoo.com>
2022-02-16 23:29:28 -0500

    the committer

--- kata.js
+++ kata.js
@@ -5456,3 +5456,11 @@ function repeats(arr) {
             };
             return result;
         };
+
+        // solution
+
+        function repeats(arr) {
+            let set = new Set()
+            arr.forEach(x => set.has(x) ? set.delete(x) : set.add(x))
+            return [...set].reduce((s, v) => s + v, 0)
+        }
