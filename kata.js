d8e9c1c331eb7cac4da6424b815cfcda05639b27
Talktovictor <itsvictornwani@yahoo.com>
2022-03-12 23:11:37 -0500

    lol

--- kata.js
+++ kata.js
@@ -5762,3 +5762,12 @@ function repeats(arr) {
                 var ks = Object.keys(helper);
                 return parseInt(ks[0]) + parseInt(ks[1]);
             };
+
+            // solution
+            function repeats(arr) {
+
+                let dup_arr = []
+                let new_arr = []
+                arr.map(n => dup_arr.includes(n) ? dup_arr.map(el => el == n && new_arr.push(el)) : dup_arr.push(n))
+                return dup_arr.filter(num => !new_arr.includes(num)).reduce((acc, cv) => acc + cv)
+            }
