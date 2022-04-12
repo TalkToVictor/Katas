745545127d343a84163bc7a4af9abb899f86a65c
Talktovictor <itsvictornwani@yahoo.com>
2022-04-11 23:57:19 -0400

    the committer

--- kata.js
+++ kata.js
@@ -6073,3 +6073,16 @@ function repeats(arr) {
             }
             return array.reduce((a, b) => +a + +b, 0)
         };
+
+        // solution
+        function repeats(arr) {
+            var helper = {};
+            for (var i = 0; i < arr.length; ++i) {
+                if (helper[arr[i]] === undefined) {
+                    helper[arr[i]] = 1;
+                } else {
+                    delete helper[arr[i]];
+                }
+            }
+            var ks = Object.keys(helper);
+            return parseInt(ks[0]) + parseInt(ks[1]);
